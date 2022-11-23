01ff2bc0a3edc38e55d77cf575d82a4564eb6b5a
pyry <pyry.kaleva@gmail.com>
2022-11-23 12:36:08 +0200

    Test

--- models/person.js
+++ models/person.js
@@ -5,10 +5,10 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url)
-  .then(result => {    
-    console.log('connected to MongoDB')  })  
-  .catch((error) => {    
-    console.log('error connecting to MongoDB:', error.message)  
+  .then(result => {
+    console.log('connected to MongoDB')  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
 })
 
 const personSchema = new mongoose.Schema({
